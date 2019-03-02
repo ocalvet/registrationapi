00de0b73f2004088b7e1973dc6cf5e49b91eeadb
Ovidio R. Calvet <calveto@gmail.com>
2019-03-02 09:23:37 -0500

    Connect to a json database

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module registration
 
 go 1.12
+
+require (
+	github.com/jcelliott/lumber v0.0.0-20160324203708-dd349441af25 // indirect
+	github.com/mdempsky/gocode v0.0.0-20190203001940-7fb65232883f // indirect
+	github.com/nanobox-io/golang-scribble v0.0.0-20180621225840-336beac0a992
+	golang.org/x/tools v0.0.0-20190228203856-589c23e65e65 // indirect
+)
--- registration.go
+++ registration.go
@@ -1,9 +1,58 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+
+	scribble "github.com/nanobox-io/golang-scribble"
 )
 
+type Idea struct {
+	ID          string
+	Title       string
+	Description string
+	Path        string
+}
+
 func main() {
-	fmt.Println("Hello")
+	dir := "./data"
+
+	db, err := scribble.New(dir, nil)
+	if err != nil {
+		fmt.Println("Error creating database file", err)
+	}
+
+	// Write a fish to the database
+	idea := Idea{
+		ID:          "1",
+		Title:       "Sample Idea",
+		Description: "A simple idea to test application",
+	}
+
+	if err := db.Write("idea", idea.ID, idea); err != nil {
+		fmt.Println("Error creating idea")
+	}
+
+	dbIdea := Idea{}
+	if err := db.Read("idea", idea.ID, &dbIdea); err != nil {
+		fmt.Println("Error reading idea", err)
+	}
+
+	fmt.Println("Found " + dbIdea.Title)
+	fmt.Println("")
+	// Read all ideas from the database, unmarshaling the response.
+	records, err := db.ReadAll("idea")
+	if err != nil {
+		fmt.Println("Error reading ideas", err)
+	}
+
+	ideas := []Idea{}
+	for _, i := range records {
+		ideaFound := Idea{}
+		if err := json.Unmarshal([]byte(i), &ideaFound); err != nil {
+			fmt.Println("Error", err)
+		}
+		ideas = append(ideas, ideaFound)
+	}
+	fmt.Println(ideas)
 }
