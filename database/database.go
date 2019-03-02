3c5848d57d70d3835be9aea569f5a67c79e24e5c
Ovidio R. Calvet <calveto@gmail.com>
2019-03-02 10:00:34 -0500

    Better module definition

--- database/database.go
+++ database/database.go
@@ -1,9 +1,11 @@
 package database
 
 import (
+	"encoding/json"
 	"fmt"
 
-	scribble "src/github.com/nanobox-io/golang-scribble"
+	scribble "github.com/nanobox-io/golang-scribble"
+	"github.com/ocalvet/registrationapi/models"
 )
 
 // StoreReader reads data from a store
@@ -13,17 +15,49 @@ type StoreReader interface {
 
 // DB is the database
 type DB struct {
-	db scribble.DB
+	db *scribble.Driver
 }
 
 // New creates a new database
 func New() DB {
 	dir := "./data"
-
-	db, err := scribble.New(dir, nil)
+	db := DB{}
+	scribble, err := scribble.New(dir, nil)
 	if err != nil {
-		fmt.Fatal("Error creating database file", err)
+		fmt.Printf("Error creating database file %v", err)
+	}
+	db.db = scribble
+	return db
+}
+
+func (db DB) AddIdea(idea models.Idea) {
+	if err := db.db.Write("idea", idea.ID, idea); err != nil {
+		fmt.Println("Error creating idea")
+	}
+}
+
+func (db DB) GetIdea(id string) models.Idea {
+	dbIdea := models.Idea{}
+	if err := db.db.Read("idea", id, &dbIdea); err != nil {
+		fmt.Println("Error reading idea", err)
+	}
+	return dbIdea
+}
+
+func (db DB) GetIdeas() []models.Idea {
+	// Read all ideas from the database, unmarshaling the response.
+	records, err := db.db.ReadAll("idea")
+	if err != nil {
+		fmt.Println("Error reading ideas", err)
 	}
 
-	return DB{}
+	ideas := []models.Idea{}
+	for _, i := range records {
+		ideaFound := models.Idea{}
+		if err := json.Unmarshal([]byte(i), &ideaFound); err != nil {
+			fmt.Println("Error", err)
+		}
+		ideas = append(ideas, ideaFound)
+	}
+	return ideas
 }
--- registration.go
+++ registration.go
@@ -1,48 +1,28 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 
-	"github.com/ocalvet/registrationapi/models"
-
 	"github.com/ocalvet/registrationapi/database"
+	"github.com/ocalvet/registrationapi/models"
 )
 
 func main() {
 	db := database.New()
+	fmt.Println(db)
 
-	// Write a fish to the database
+	// Add an idea to the database
 	idea := models.Idea{
 		ID:          "1",
 		Title:       "Sample Idea",
 		Description: "A simple idea to test application",
 	}
 
-	if err := db.Write("idea", idea.ID, idea); err != nil {
-		fmt.Println("Error creating idea")
-	}
+	db.AddIdea(idea)
 
-	dbIdea := models.Idea{}
-	if err := db.Read("idea", idea.ID, &dbIdea); err != nil {
-		fmt.Println("Error reading idea", err)
-	}
+	i := db.GetIdea(idea.ID)
+	fmt.Println("Found " + i.Title)
 
-	fmt.Println("Found " + dbIdea.Title)
-	fmt.Println("")
-	// Read all ideas from the database, unmarshaling the response.
-	records, err := db.ReadAll("idea")
-	if err != nil {
-		fmt.Println("Error reading ideas", err)
-	}
-
-	ideas := []models.Idea{}
-	for _, i := range records {
-		ideaFound := models.Idea{}
-		if err := json.Unmarshal([]byte(i), &ideaFound); err != nil {
-			fmt.Println("Error", err)
-		}
-		ideas = append(ideas, ideaFound)
-	}
+	ideas := db.GetIdeas()
 	fmt.Println(ideas)
 }
