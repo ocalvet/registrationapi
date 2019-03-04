667556771256e1cd0cc40c9e66d0de2443969a61
Ovidio R. Calvet <calveto@gmail.com>
2019-03-03 21:54:31 -0500

    Use a mux

--- registration.go
+++ registration.go
@@ -2,27 +2,55 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
+	"strings"
 
 	"github.com/ocalvet/registrationapi/database"
-	"github.com/ocalvet/registrationapi/models"
 )
+
+func generateIdeaHandler() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		switch r.Method {
+		case http.MethodGet:
+			id := strings.TrimPrefix(r.URL.Path, "/api/ideas/")
+			if len(id) > 0 {
+				log.Printf("Id %s", id)
+				fmt.Fprintf(w, "Hi With ID: %s", id)
+			} else {
+				log.Println("Handling GET")
+				fmt.Fprint(w, "Hi Without ID")
+			}
+		case http.MethodPost:
+			log.Println("Handling POST")
+			fmt.Fprint(w, "Hi Post")
+		default:
+			w.WriteHeader(404)
+			fmt.Fprintf(w, "Wrong method: %s", r.Method)
+		}
+	}
+}
 
 func main() {
 	db := database.New()
 	fmt.Println(db)
 
-	// Add an idea to the database
-	idea := models.Idea{
-		ID:          "1",
-		Title:       "Sample Idea",
-		Description: "A simple idea to test application",
-	}
+	// // Add an idea to the database
+	// idea := models.Idea{
+	// 	ID:          "1",
+	// 	Title:       "Sample Idea",
+	// 	Description: "A simple idea to test application",
+	// }
 
-	db.AddIdea(idea)
+	// db.AddIdea(idea)
 
-	i := db.GetIdea(idea.ID)
-	fmt.Println("Found " + i.Title)
+	// i := db.GetIdea(idea.ID)
+	// fmt.Println("Found " + i.Title)
 
-	ideas := db.GetIdeas()
-	fmt.Println(ideas)
+	// ideas := db.GetIdeas()
+	// fmt.Println(ideas)
+	mux := http.NewServeMux()
+	mux.HandleFunc("/api/ideas/", generateIdeaHandler())
+	log.Println("Listening :8080")
+	log.Fatal(http.ListenAndServe(":8080", mux))
 }
