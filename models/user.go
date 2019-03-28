53f0d5717bce53aa104b591e3710f70baebc6040
Ismail Fletcher <ismail_fletcher_medina@ncci.com>
2019-03-28 18:36:24 -0400

    Removed trash for testing

--- models/user.go
+++ models/user.go
@@ -1,13 +1,10 @@
 package models
 
 // User Who is registering?
-saldklsad
-kdslakjd;lsks
-lksajdlsa
 type User struct {
 	FirstName string   `json:"firstName"`
 	LastName  string   `json:"lastName"`
 	Email     string   `json:"email"`
-skdajlsadj	Skills    []string `json:"skills"`
+	Skills    []string `json:"skills"`
 	Equipment string   `json:"equipment"`
 }
